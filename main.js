a6c1bb0452f37aa03885f69c772c4248a258387c
Ranjith-mylsamy <ranjithmylsamy@gmail.com>
2022-09-05 01:51:45 +0530

    bug fix 1

--- main.js
+++ main.js
@@ -1,33 +1,11 @@
 $(document).ready(function(){
-    $("#loginImg").css('display', 'none');
       $('#menu').click(function(){
           $(this).toggleClass('fa-times');
           $('.navbar').toggleClass('nav-toggle');
       });
-      $('#login').click(function(){
-          $('.login-form').addClass('popup');
-      });
-      $('.login-form form .fa-times').click(function(){
-          $('.login-form').removeClass('popup');
-      });
-      $('.login-form ul .signup').click(function(){
-        $('.sign-up-form').addClass('popup');
-     });
-     $('.sign-up-form form .fa-times').click(function(){
-        $('.sign-up-form').removeClass('popup');
-     });
-     $('.login-form form .btn').click(function(event){
-      event.preventDefault();
-      login() 
-     });
-      $('.login-form .sign-in-with-google p').click(function(event){
-          event.preventDefault();
-          signin() 
-      });
       $(window).on('load scroll',function(){
               $('#menu').removeClass('fa-times');
               $('.navbar').removeClass('nav-toggle');
-              $('.login-form').removeClass('popup'); 
               $('section').each(function(){
                   let top =$(window).scrollTop();
                   let height=$(this).height();
