90dfd753e8fd0f25c0339ddcf7f320995ae6c94d
Ranjith-mylsamy <ranjithmylsamy@gmail.com>
2022-09-05 02:45:29 +0530

    js

--- main.js
+++ main.js
@@ -6,17 +6,5 @@ $(document).ready(function(){
       $(window).on('load scroll',function(){
               $('#menu').removeClass('fa-times');
               $('.navbar').removeClass('nav-toggle');
-              $('section').each(function(){
-                  let top =$(window).scrollTop();
-                  let height=$(this).height();
-                  let id=$(this).attr('id');
-                  let offset=$(this).offset().top - 200;
-                  
-                  if(top > offset && top < offset + height){
-                      $('.navbar ul li a').removeClass('active');
-                      $('.navbar').find(`[href="#${id}"]`).addClass('active');
-                  }
               });
       });
-  });
-  
