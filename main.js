d06bf23f330d985b6f8479e9c3936e6bdb65521f
Ranjith-mylsamy <ranjithmylsamy@gmail.com>
2022-09-25 17:02:45 +0530

    js for animate

--- main.js
+++ main.js
@@ -3,3 +3,6 @@ document.querySelector("#menu").addEventListener("click",()=>{
     document.querySelector("nav").classList.toggle("nav-toggle");
     document.querySelector("#menu").classList.toggle("fa-times");
 })
+const signupbutton=document.getElementById("signup");
+const signinbutton=document.getElementById("signup");
+const container=document.getElementById("")
