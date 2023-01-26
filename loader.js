9aa01a7158b60ef35ddf120f2a2e6ff76a5ce4e6
Ranjith-mylsamy <ranjithmylsamy@gmail.com>
2023-01-26 14:09:18 +0530

    gmail test

--- loader.js
+++ loader.js
@@ -2,8 +2,8 @@ let loader=document.querySelector(".sec-06")
 // var styleWebkit=document.createElement("style");
 // styleWebkit.appendChild(document.createTextNode("html::-webkit-scrollbar{width:1rem;}html::-webkit-scrollbar-track{background:var(--blue2);}html::-webkit-scrollbar-thumb{background:var(--gradient);border-radius:10px;}"));
 // document.getElementsByTagName("head")[0].appendChild(styleWebkit);
-let scrollbar=document.getElementsByTagName("html");
-scrollbar.style.overflowY="hidden";
+// let scrollbar=document.getElementsByTagName("html");
+// scrollbar.style.overflowY="hidden";
 window.onload = function(){
     loader.style.display="none";
 }
