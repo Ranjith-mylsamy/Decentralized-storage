e263d1bdf3294c6521c41124deb42f637ecaf5af
Ranjith-mylsamy <ranjithmylsamy@gmail.com>
2023-01-29 21:06:47 +0530

    google auth

--- src/firebase.js
+++ src/firebase.js
@@ -62,3 +62,28 @@ const logoutButton = document.getElementById("signout");
   onAuthStateChanged(auth,(user) => {
     console.log('user status changed: ',user);
   })
+  //google sign up
+
+const provider = new GoogleAuthProvider();
+import {signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+const googleBtn=document.getElementById("google");
+googleBtn.addEventListener('click',function(){
+signInWithPopup(auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });});
