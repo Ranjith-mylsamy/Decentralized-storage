0c1b584c6683afafa6a8414f44d8ea50cc159c3c
Ranjith-mylsamy <ranjithmylsamy@gmail.com>
2023-02-01 18:59:32 +0530

    updated loader

--- loader.js
+++ loader.js
@@ -1,7 +1,7 @@
-let loader=document.querySelector(".sec-06")
 // var styleWebkit=document.createElement("style");
 // styleWebkit.appendChild(document.createTextNode("html::-webkit-scrollbar{width:1rem;}html::-webkit-scrollbar-track{background:var(--blue2);}html::-webkit-scrollbar-thumb{background:var(--gradient);border-radius:10px;}"));
 // document.getElementsByTagName("head")[0].appendChild(styleWebkit);
+let loader=document.querySelector(".sec-06");
 let scrollbar=document.getElementsByTagName("html");
 window.onload = function(){
     loader.style.display="none";
