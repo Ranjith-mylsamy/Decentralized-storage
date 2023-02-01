0b18ed8766388b680459db417236e88f1e35cef3
Ranjith-mylsamy <ranjithmylsamy@gmail.com>
2023-02-01 18:30:18 +0530

    loader js

--- loader.js
+++ loader.js
@@ -2,10 +2,9 @@ let loader=document.querySelector(".sec-06")
 // var styleWebkit=document.createElement("style");
 // styleWebkit.appendChild(document.createTextNode("html::-webkit-scrollbar{width:1rem;}html::-webkit-scrollbar-track{background:var(--blue2);}html::-webkit-scrollbar-thumb{background:var(--gradient);border-radius:10px;}"));
 // document.getElementsByTagName("head")[0].appendChild(styleWebkit);
-// let scrollbar=document.getElementsByTagName("html");
-// scrollbar.style.overflowY="hidden";
+let scrollbar=document.getElementsByTagName("html");
 window.onload = function(){
     loader.style.display="none";
+    document.getElementsByTagName('html')[0].style.overflowY = "scroll";
 }
-scrollbar.tagName.style
 //setting time out is the solution sample
