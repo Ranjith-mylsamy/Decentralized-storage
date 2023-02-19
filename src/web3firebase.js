48a8752c6fe65c7a4697bacfce87306a86980e6e
Ranjith-mylsamy <ranjithmylsamy@gmail.com>
2023-02-19 22:01:26 +0530

    firebase init in web3

--- src/web3firebase.js
+++ src/web3firebase.js
@@ -1,4 +1,21 @@
 import { Web3Storage } from 'web3.storage';
+import { initializeApp } from 'firebase/app';
+import { 
+  getFirestore,collection,getDocs
+} from 'firebase/firestore'
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCmvBcRblmn4O0yx1UAetfPnyMRgqC8C-4",
+  authDomain: "web3storage-5f50b.firebaseapp.com",
+  databaseURL: "https://web3storage-5f50b-default-rtdb.firebaseio.com",
+  projectId: "web3storage-5f50b",
+  storageBucket: "web3storage-5f50b.appspot.com",
+  messagingSenderId: "709180782228",
+  appId: "1:709180782228:web:b479d09d2055d2dc54e681",
+  measurementId: "G-ELP48BVPP3"
+};
+
 function getAccessToken () {
     const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDc1Yjc3RThhNmRCMTNlNjhmZThlMUMwMzEwMDk4QUNlNEZFN2RBNWEiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NzYxOTk5Nzk1MDEsIm5hbWUiOiJ3ZWIzIn0.IUPdIPd94UXhLK8HWzyeZ7rDLFtzj2DWmAlm0t8LDkM";
     return token
@@ -22,7 +39,7 @@ async function StoreFiles (file) {
         console.log('uploading files with cid:', cid)
     }
 
-    const totalSize = file.size;
+    const totalSize = file[0].size;
     let uploaded = 0;
     const onStoredChunk = size => {
         uploaded += size
@@ -65,3 +82,15 @@ async function storefilesinW3andFirebase(){
         console.log("Please select the File");
     }
 };
+
+//firebase part
+const app = initializeApp(firebaseConfig);
+
+//init services
+const db = getFirestore()
+//collection ref
+const colRef = collection(db,'W3Data')
+//get collection data
+getDocs(colRef).then((snapshot)=>{
+  console.log(snapshot.docs);
+})
